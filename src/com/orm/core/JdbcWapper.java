09576269676060afd129b9a2da193f1d56beaebe
tlhhup <137273278@qq.com>
2016-07-19 10:46:03 +0800

    处理读取配置文件

--- src/com/orm/core/JdbcWapper.java
+++ src/com/orm/core/JdbcWapper.java
@@ -16,7 +16,11 @@ public abstract class JdbcWapper {
 
 	public JdbcWapper() {
 		// 默认的配置文件
-		InputStream is = JdbcWapper.class.getResourceAsStream("jdbc.properties");
+		// 1、当前项目的根目录加载配置文件
+		InputStream is = JdbcWapper.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		if (is == null) {
+			is = JdbcWapper.class.getResourceAsStream("jdbc.properties");
+		}
 		Properties properties = new Properties();
 		try {
 			properties.load(is);
@@ -46,22 +50,25 @@ public abstract class JdbcWapper {
 
 	/**
 	 * 查询数据
-	 * @param sql 执行的sql
-	 * @param values 查询条件的值
+	 * 
+	 * @param sql
+	 *            执行的sql
+	 * @param values
+	 *            查询条件的值
 	 * @return
 	 * @throws Exception
 	 */
 	protected ResultSet executeQurey(String sql, List<Object> values) throws Exception {
 		try {
 			statement = connection.prepareStatement(sql);
-			if (values != null) {//排除没有查询条件
+			if (values != null) {// 排除没有查询条件
 				// 绑定数据
 				for (int i = 1; i <= values.size(); i++) {
 					statement.setObject(i, values.get(i - 1));
 				}
 			}
 			return statement.executeQuery();
-		} catch (Exception e){
+		} catch (Exception e) {
 			throw e;
 		}
 
